97490b4a3f715b0f06fad972bd0bf82ad2618cfe
phemidawhiz <feminiscence@gmail.com>
2020-06-02 14:39:22 +0100

    create persons component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent
   ],
   imports: [
     BrowserModule,
