2e601ea4579a25f20bc00c6942a131f234b62eaf
phemidawhiz <feminiscence@gmail.com>
2020-06-05 19:27:31 +0100

    Handling user input with event and two way binding

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -2,9 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-persons',
-    templateUrl: './persons.component.html'
+    templateUrl: './persons.component.html',
+    styleUrls: ['./persons.component.css']
 })
 
 export class PersonsComponent {
+    enteredPersonName = '';
     @Input() personList: string[];
+
+    onCreatePerson() {
+        console.log("Person created: ", this.enteredPersonName);
+        this.enteredPersonName = '';
+    }
 }
