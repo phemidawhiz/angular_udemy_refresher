7bdbb4a97724619dce6ea97f2d92be113b909de0
phemidawhiz <feminiscence@gmail.com>
2020-06-05 20:47:39 +0100

    passing data with custom event using output properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'udemy';
   persons: string[] = ['Adeolu', 'Jackson', 'Edith'];
+
+  onPersonCreated(name: string) {
+    this.persons.push(name);
+  }
 }
--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-persons',
@@ -9,9 +9,12 @@ import { Component, Input } from '@angular/core';
 export class PersonsComponent {
     enteredPersonName = '';
     @Input() personList: string[];
+    @Output() PersonCreate = new EventEmitter<string>()
 
     onCreatePerson() {
-        console.log("Person created: ", this.enteredPersonName);
+        console.log('Person created: ', this.enteredPersonName);
+        this.PersonCreate.emit(this.enteredPersonName);
         this.enteredPersonName = '';
+
     }
 }
